244a42d7df3e62a01371fc3fd189fa452609f2ac
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-15 10:04:26 +0600

    showed how to use onclick using function

--- js/events.js
+++ js/events.js
@@ -1,1 +1,8 @@
 console.log("file connected")
+       function makeYellow(){
+            document.body.style.backgroundColor='yellow';
+        }
+
+        function makeRed(){
+            document.body.style.backgroundColor='red';
+        }
