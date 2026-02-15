9cf75d12dfa4d597d51804fc8794a2e376fe2f8f
zisanahmed97 <zisanahmed97@gmail.com>
2026-02-15 10:11:02 +0600

    showed option 3 of onclick set

--- js/events.js
+++ js/events.js
@@ -1,4 +1,7 @@
 console.log("file connected")
+
+
+//option 2 
        function makeYellow(){
             document.body.style.backgroundColor='yellow';
         }
@@ -6,3 +9,10 @@ console.log("file connected")
         function makeRed(){
             document.body.style.backgroundColor='red';
         }
+
+
+//option 3
+const btnMakeGreen= document.getElementById('btn-make-green');
+btnMakeGreen.onclick=function makeGreen(){
+    document.body.style.backgroundColor='green';
+}
